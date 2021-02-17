6fbc344e925fe8a0d2d2a1f409a93357093608e3
diazdez <69435783+diazdez@users.noreply.github.com>
2021-02-16 21:26:29 -0600

    adding procfile, reqs.txt, template, app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+# IMPORTS
+
+# import os
+from flask import (
+    Flask,
+    render_template)
+
+
+# Flask Setup
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
